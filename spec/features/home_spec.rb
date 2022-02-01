43f2c57eca6eec45f3bdcf9d95b88a11bc500ec4
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-01-31 21:57:20 -0500

    Feature complete landing page

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'home page' do
+  it 'has a link to return to the landing page' do
+    visit ('/')
+    expect(page).to have_link('Return to home')
+    expect(page).to have_content('Viewing Party')
+  end
+
+  it 'has links to user dashboards' do
+    user1 = User.create!(name: 'Joe Schmoe', email: 'jschmoe@mail.com')
+    user2 = User.create!(name: 'Jane Schmoe', email: 'janeschmoe@mail.com')
+    user3 = User.create!(name: 'Moe Schmoe', email: 'moeshmoe@mail.com')
+    visit ('/')
+
+    within(".existing-users") do
+      expect(page).to have_link("#{user1.email}'s Dashboard")
+    end
+    click_link "#{user1.email}'s Dashboard"
+    expect(current_path).to eq("/users/#{user1.id}")
+  end
+
+  it 'has a create user button' do
+    visit ('/')
+    expect(page).to have_button('Create a New User')
+
+    click_button 'Create a New User'
+    expect(current_path).to eq('/register')
+  end
+end
