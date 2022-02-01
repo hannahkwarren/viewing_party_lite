4c69c216fb3b464b467d6568b2169fd5fea7989a
Hannah Warren <hannahkwarren@gmail.com>
2022-02-01 14:58:59 -0500

    Add validation for email uniqueness on User model, with flash messaging

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,13 @@ class UsersController < ApplicationController
 
   def create
     user = User.create(user_params)
-    redirect_to user_path(user)
+    if user.save
+      flash[:success] = "User created!"
+      redirect_to user_path(user)
+    else
+      flash[:alert] = "#{user_params[:email]} has already been taken, please try another email address."
+      redirect_to "/register"
+    end
   end
 
   private
