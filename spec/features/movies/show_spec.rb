48fb67a0b4727e92fe8a1df31375e2c09035d529
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-03 10:53:20 -0500

    Add more tests for additional API endpoints

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -1,31 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "Movie Details page", type: :feature do 
+RSpec.describe 'Movie Details page', type: :feature do
+  it 'has buttons to create a Viewing Party and to return to Discover' do
+    user1 = User.create!(name: 'Joe Schmoe', email: 'joeschmoe@mail.com')
 
-  it "has buttons to create a Viewing Party and to return to Discover" do 
-    user1 = User.create!(name: "Joe Schmoe", email: "joeschmoe@mail.com")
-    
     visit "/users/#{user1.id}/movies/497698"
-    
-    expect(page).to have_button("Discover Page")
-    expect(page).to have_button("Create Viewing Party for Black Widow")
+
+    expect(page).to have_button('Discover Page')
+    expect(page).to have_button('Create Viewing Party for Black Widow')
   end
 
-  it "Movie Details page should have movie data" do
-    VCR.use_cassette('movie_db_discover_search') do
-        
-      user1 = User.create!(name: "Joe Schmoe", email: "joeschmoe@mail.com")
-      
+  it 'Movie Details page should have movie data' do
+    VCR.use_cassette('movie_db_discover_details') do
+      user1 = User.create!(name: 'Joe Schmoe', email: 'joeschmoe@mail.com')
+
       visit "/users/#{user1.id}/movies/497698"
-      
-      expect(page).to have_content("Black Widow")
-      expect(page).to have_content("Vote Average: 7.5")
-      expect(page).to have_content("Genre(s): Action, Adventure, Science Fiction")
+      expect(page).to have_content('Black Widow')
+      expect(page).to have_content('Vote Average: 7.5')
+      expect(page).to have_content('Genre(s): Action, Adventure, Science Fiction')
       expect(page).to have_content("Summary:\nNatasha Romanoff, also known as Black Widow")
+      expect(page).to have_content("Cast:\nScarlett Johansson as Natasha Romanoff / Black Widow")
+      expect(page).to have_content('10 Reviews')
+      expect(page).to have_content('Author: Trevfh')
+      expect(page).to have_content("Author: Trevfh\nIt was okay")
     end
-
   end
-
-  
-
 end
