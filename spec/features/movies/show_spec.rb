e940c30a1de58f24ca3fe85983a9447719354cf0
Hannah Warren <hannahkwarren@gmail.com>
2022-02-02 20:04:03 -0500

    Add directories and start specs

---
+++ spec/features/movies/show_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "Movie Details page", type: :feature do 
+
+  it "has buttons to create a Viewing Party and to return to Discover" do 
+
+  end
+
+  xit "Movie Details page should have movie data" do
+  end
+
+  xit ""
+
+
+
+end
