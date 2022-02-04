6a41720a76ff38ce8232570066e8f1ad19db01f1
Hannah Warren <hannahkwarren@gmail.com>
2022-02-04 10:36:00 -0500

    Adds Movie Service

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,21 +18,7 @@ class MoviesController < ApplicationController
      @movie = MovieFacade.movie_data(params[:movie_id])
      @credits = MovieFacade.credits(params[:movie_id])
      @movie_reviews = MovieFacade.reviews(params[:movie_id])
-    # response = connection.get("movie/#{params[:movie_id]}") do |request|
-    #   request.params['api_key'] = ENV['movies_api_key']
-    # end
-    # response2 = connection.get("movie/#{params[:movie_id]}/credits") do |request|
-    #   request.params['api_key'] = ENV['movies_api_key']
-    # end
-    # response3 = connection.get("movie/#{params[:movie_id]}/reviews") do |request|
-    #   request.params['api_key'] = ENV['movies_api_key']
-    # end
-
-    # @user = User.find(params[:id])
-    # @movie = JSON.parse(response.body, symbolize_names: true)
-    # @movie_credits = JSON.parse(response2.body, symbolize_names: true)
-    # @movie_reviews = JSON.parse(response3.body, symbolize_names: true)
-
+    
     render '/users/details'
   end
 end
