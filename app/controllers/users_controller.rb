696783651b45a997b8ef2422097c8333f6c58c1e
Arnaldo Aparicio <apaarn2211@gmail.com>
2022-02-04 12:58:21 -0500

    Refactor create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-    @user_party = UserParty.all
-    # binding.pry
+    @user = User.find(params[:id]) 
   end
 
   def new
