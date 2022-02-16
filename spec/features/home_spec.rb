c181990c8404b567bd43c4cbf42cc02595d9ae52
Hannah Warren <hannahkwarren@gmail.com>
2022-02-16 16:54:42 -0500

    Gradual refactor

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,30 +1,36 @@
+# spec/features/home_spec.rb
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'home page', type: :feature do
   it 'has a link to return to the landing page' do
-    visit ('/')
+    visit root_path
     expect(page).to have_link('Return to home')
     expect(page).to have_content('Viewing Party')
   end
-  
-  it 'has a create user button' do
-    visit ('/')
-    expect(page).to have_button('Create a New User')
-    
-    click_button('Create a New User')
-    expect(current_path).to eq('/register')
-  end
-  
-  it 'has links to user dashboards' do
-    user1 = User.create!(name: 'Joe Schmoe', email: 'jschmoe@mail.com')
-    user2 = User.create!(name: 'Jane Schmoe', email: 'janeschmoe@mail.com')
-    user3 = User.create!(name: 'Moe Schmoe', email: 'moeshmoe@mail.com')
-    visit ('/')
 
-    within(".existing-users") do
+  it 'has a register user button' do
+    visit root_path
+    expect(page).to have_button('Register as a User')
+
+    click_button('Register as a User')
+    expect(current_path).to eq(register_path)
+  end
+
+  it 'has links to user dashboards' do
+    user1 = User.create!(name: 'Joe Schmoe', email: 'jschmoe@mail.com', password: 'test123', password_confirmation: 'test123')
+    user2 = User.create!(name: 'Jane Schmoe', email: 'janeschmoe@mail.com', password: 'test123', password_confirmation: 'test123')
+    user3 = User.create!(name: 'Moe Schmoe', email: 'moeshmoe@mail.com', password: 'test123', password_confirmation: 'test123')
+    visit root_path
+
+    within('.existing-users') do
       expect(page).to have_link("#{user1.email}'s Dashboard")
+      expect(page).to have_link("#{user2.email}'s Dashboard")
+      expect(page).to have_link("#{user3.email}'s Dashboard")
     end
+
     click_link "#{user1.email}'s Dashboard"
-    expect(current_path).to eq("/users/#{user1.id}")
+    expect(current_path).to eq(user_path(user1))
   end
 end
